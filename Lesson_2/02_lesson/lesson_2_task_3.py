81cb502507b1f055b16535af3c9dac7436cd0ca0
Alla <alla.morozova@mail.ru>
2026-01-11 17:13:42 +0300

    Сделала вторую домашку

---
+++ Lesson_2/02_lesson/lesson_2_task_3.py
@@ -0,0 +1,7 @@
+#Напишите функцию square, принимающую один аргумент — сторону квадрата — и возвращающую площадь квадрата.
+
+def square(side):
+    return side * side
+side = 5
+area = square(side)
+print(area)
