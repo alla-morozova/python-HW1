d032e0389ae7be337f08ee594e235bdce1fab5fc
Alla <alla.morozova@mail.ru>
2026-02-15 23:16:34 +0300

    Поправила код в 6 домашке

--- 06_Lesson/rename_button.py
+++ 06_Lesson/rename_button.py
@@ -4,23 +4,25 @@
 #Нажмите на синюю кнопку.
 #Получите текст кнопки и выведите в консоль ("SkyPro")
 
-
-from time import sleep #импортировали метод из пакета
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.common.by import By #не забудьте импортировать класс By
-
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 driver = webdriver.Chrome(
 service=ChromeService(ChromeDriverManager().install()))
 
-driver.get(" http://uitestingplayground.com/textinput")
+driver.get("http://uitestingplayground.com/textinput")
 
 element = driver.find_element(By.ID, "newButtonName")
 element.send_keys("SkyPro")
 
 element2 = driver.find_element(By.CSS_SELECTOR, ".btn.btn-primary")
 element2.click()
+
+wait = WebDriverWait(driver, 20)
+element3 = wait.until(EC.visibility_of_element_located((By.ID, "updatingButton")))
 
 button_text = element2.text
 print(button_text)
--- 06_Lesson/wait_for_the_picture.py
+++ 06_Lesson/wait_for_the_picture.py
@@ -9,7 +9,7 @@ from time import sleep
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.common.by import By #не забудьте импортировать класс By
+from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 driver = webdriver.Chrome(
@@ -17,13 +17,13 @@ service=ChromeService(ChromeDriverManager().install()))
 
 driver.get(" https://bonigarcia.dev/selenium-webdriver-java/loading-images.html")
 
-waiter = WebDriverWait(driver, 40)
+waiter = WebDriverWait(driver,40)
 images1 = waiter.until(EC.visibility_of_element_located((By.ID, "compass")))
 images2 = waiter.until(EC.visibility_of_element_located((By.ID, "calendar")))
 images3 = waiter.until(EC.visibility_of_element_located((By.ID, "award")))
 images4 = waiter.until(EC.visibility_of_element_located((By.ID, "landscape")))
 
-src_images3 = driver.find_element(By.ID, "award").get_attribute("src")
+src_images3 = driver.find_element(By.ID,"award").get_attribute("src")
 print(src_images3)
 
 driver.quit()
